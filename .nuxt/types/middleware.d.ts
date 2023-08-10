7b7b3ef33ab8ac8df622153a0276b361926e3007
TaeTanakrit <65070089@kmitl.ac.th>
2023-08-10 17:01:51 +0700

    Reformatted Code

---
+++ .nuxt/types/middleware.d.ts
@@ -0,0 +1,8 @@
+import type {NavigationGuard} from 'vue-router'
+
+export type MiddlewareKey = string
+declare module "/Users/tanakritsupprasit/WebstormProjects/db-sus-project/node_modules/nuxt/dist/pages/runtime/composables" {
+    interface PageMeta {
+        middleware?: MiddlewareKey | NavigationGuard | Array<MiddlewareKey | NavigationGuard>
+    }
+}
--- Frontend/pages/account/index.vue
+++ Frontend/pages/account/index.vue
@@ -3,10 +3,10 @@
   <v-btn v-on:click="() => $router.push( {name: 'index'} )">
     This is Button
   </v-btn>
-  <v-img
-      alt="ejudge"
-      class="mx-auto my-4 rounded-lg border"
-      src="https://ejudge.it.kmitl.ac.th/uploads/4496_1670336142_tP3P4Svj.jpg"
-      width="20%"
-  ></v-img>
+<!--  <v-img-->
+<!--      alt="ejudge"-->
+<!--      class="mx-auto my-4 rounded-lg border"-->
+<!--      src="https://ejudge.it.kmitl.ac.th/uploads/4496_1670336142_tP3P4Svj.jpg"-->
+<!--      width="20%"-->
+<!--  ></v-img>-->
 </template>

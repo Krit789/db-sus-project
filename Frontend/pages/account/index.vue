403c9c0d5a2a836c51050b833c4ef0e26ffcdd7d
japan0101 <japan.pon11@gmail.com>
2023-08-17 15:08:16 +0700

    Account page layout fix

--- Frontend/pages/account/index.vue
+++ Frontend/pages/account/index.vue
@@ -7,14 +7,17 @@ const {
 
 <template>
   <Navbar>
-    <div v-if="status == 'authenticated'">
-      <h1>Welcome to Account</h1>
-      <p>First Name: {{ data.firstName }}</p>
-      <p>Last Name: {{ data.lastName }}</p>
-      <p>Tel Number: {{ data.tel }}</p>
-    </div>
-    <v-btn v-on:click="() => $router.push({ name: 'index' })">
-      Back to Index
-    </v-btn>
+    <v-main>
+      <div v-if="status == 'authenticated'">
+        <h1>Welcome to Account</h1>
+        <p>First Name: {{ data.firstName }}</p>
+        <p>Last Name: {{ data.lastName }}</p>
+        <p>Tel Number: {{ data.tel }}</p>
+      </div>
+      <v-btn v-on:click="() => $router.push({ name: 'index' })">
+        Back to Index
+      </v-btn>
+      
+    </v-main>
   </Navbar>
 </template>

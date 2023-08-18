6ec391ecaa0da0cddd45cd37c5a01a541a6ca841
japan0101 <japan.pon11@gmail.com>
2023-08-18 16:55:11 +0700

    Add register dialogue box
    Not yet finished, no have function and can look better

--- Frontend/components/Navbar.vue
+++ Frontend/components/Navbar.vue
@@ -63,8 +63,8 @@ export default {
 
         <v-btn icon="mdi-dots-vertical" variant="text"></v-btn> -->
         <div v-if="status == 'unauthenticated'">
-          <v-btn color="blue" variant="text" @click="dialog = true">Register</v-btn>
-          <v-btn background-color="#D9D9D9" @click="dialog = true">Login</v-btn>
+          <v-btn color="blue" variant="text" @click="dialogRe = true">Register</v-btn>
+          <v-btn background-color="#D9D9D9" @click="dialogIn = true">Login</v-btn>
         </div>
         <div v-else-if="status == 'authenticated'">
           <v-btn variant="text">
@@ -80,7 +80,7 @@ export default {
 
       <!-- <v-main color="#D9D9D9" style="height: 1100px;"> under construction-->
         <div class="text-center">
-          <v-dialog v-model="dialog" width="auto">
+          <v-dialog v-model="dialogIn" width="auto">
             <v-card>
               <v-card-text>
                 <h1>Sign In</h1>
@@ -95,12 +95,49 @@ export default {
                 </v-sheet>
               </v-card-text>
               <v-card-actions>
-                <v-btn block color="primary" @click="dialog = false">Cancel</v-btn>
+                <v-btn block color="primary" @click="dialogIn = false">Cancel</v-btn>
               </v-card-actions>
             </v-card>
           </v-dialog>
         </div>
-        
+        <v-dialog v-model="dialogRe" width="auto">
+            <v-card>
+              <v-card-text>
+                <h1>Register</h1>
+                <v-sheet class="mx-auto" width="600">
+                  <v-form @submit.prevent>
+                    <v-row>
+                      <v-col>
+                        <v-text-field v-model="first_name" label="First Name"></v-text-field>
+                      </v-col>
+                      <v-col>
+                        <v-text-field v-model="last_name" label="Last Name"></v-text-field>
+                      </v-col>
+                      <v-responsive width="100%"></v-responsive>
+                      <v-col>
+                        <v-text-field v-model="email" label="Email"></v-text-field>
+                      </v-col>
+                      <v-responsive width="100%"></v-responsive>
+                      <v-col>
+                        <v-text-field v-model="password" label="Password" type="password"></v-text-field>
+                      </v-col>
+                      <v-col>
+                        <v-text-field v-model="confirm_password" label="Confirm Password" type="password"></v-text-field>
+                      </v-col>
+                  </v-row>
+                    <!-- <v-btn block class="mt-2 bg-blue-darken-1" type="submit"
+                           @click="signIn('credentials', { email: email, password: password, callbackUrl: '/' })">Submit -->
+                    <v-btn block class="mt-2 bg-blue-darken-1">
+                      Submit
+                    </v-btn>
+                  </v-form>
+                </v-sheet>
+              </v-card-text>
+              <v-card-actions>
+                <v-btn block color="primary" @click="dialogRe = false">Cancel</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
       <!-- </v-main> -->
       
       <slot/>

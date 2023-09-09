4be6c52fc5a1862e0cdd887aff12a4ad769bc890
japan0101 <japan.pon11@gmail.com>
2023-09-09 20:42:12 +0700

    Now you see us

--- Frontend/components/Credit.vue
+++ Frontend/components/Credit.vue
@@ -15,7 +15,7 @@
     <v-row no-gutters>
       <v-col :cols="credCols[0]">
         <v-card class="ma-2 pa-2">
-          <template v-slot:subtitle>
+          <template v-slot:title>
             <v-avatar image="/aboutus/05_Mark.webp"></v-avatar>
             <span class="font-weight-bold"> 65070005 นายกนกพล โป้ยขำ</span><div class="pt-1"><br />Some contact infomation idk</div>
           </template>
@@ -23,7 +23,7 @@
       </v-col>
       <v-col :cols="credCols[1]">
         <v-card class="ma-2 pa-2">
-          <template v-slot:subtitle>
+          <template v-slot:title>
             <v-avatar class="profile_img" image="/aboutus/28_Bess.webp"></v-avatar>
             <span class="font-weight-bold"> 65070028 นายคณิศร สมศรีอักษรแสง </span><div class="pt-1"><br />Some contact infomation idk</div>
           </template>
@@ -32,7 +32,7 @@
       <!-- <v-responsive width="100%"></v-responsive> -->
       <v-col :cols="credCols[2]">
         <v-card class="ma-2 pa-2">
-          <template v-slot:subtitle>
+          <template v-slot:title>
             <v-avatar image="/aboutus/30_Krit.webp"></v-avatar>
             <span class="font-weight-bold"> 65070030 นายจารุกิตติ์ ศรีพาเพลิน</span><div class="pt-1"><br />Some contact infomation idk</div>
           </template>
@@ -40,7 +40,7 @@
       </v-col>
       <v-col :cols="credCols[3]">
         <v-card class="ma-2 pa-2">
-          <template v-slot:subtitle>
+          <template v-slot:title>
             <v-avatar image="/aboutus/35_Jai.webp"></v-avatar>
             <span class="font-weight-bold"> 65070035 นายจิรโชติ อินทรวงษ์โชติ</span><div class="pt-1"><br />Some contact infomation idk</div>
           </template>
@@ -49,7 +49,7 @@
       <!-- <v-responsive width="100%"></v-responsive> -->
       <v-col :cols="credCols[4]">
         <v-card class="ma-2 pa-2">
-          <template v-slot:subtitle>
+          <template v-slot:title>
             <v-avatar image="/aboutus/64_Japan.webp"></v-avatar>
             <span class="font-weight-bold"> 65070064 นายณภัทร เวชพันธุ์</span><div class="pt-1"><br />Some contact infomation idk</div>
           </template>
@@ -57,7 +57,7 @@
       </v-col>
       <v-col :cols="credCols[5]">
         <v-card class="ma-2 pa-2">
-          <template v-slot:subtitle>
+          <template v-slot:title>
             <v-avatar image="/aboutus/89_Tae.webp"></v-avatar>
             <span class="font-weight-bold"> 65070085 นายธนกฤต ทรัพย์ประสิทธิ์</span><div class="pt-1"><br />Some contact infomation idk</div>
           </template>

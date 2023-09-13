d30dac2ff1e41535056106a37e233470ddc68ec2
Jarukrit <jarukritsripaploen@protonmail.com>
2023-09-14 02:41:37 +0700

    Enable Treeshaking optimisation

--- Frontend/plugins/vuetify.ts
+++ Frontend/plugins/vuetify.ts
@@ -1,6 +1,4 @@
 import {createVuetify} from 'vuetify'
-import * as components from "vuetify/components";
-import * as directives from "vuetify/directives";
 import {md3} from 'vuetify/blueprints';
 import '@mdi/font/css/materialdesignicons.css';
 
@@ -9,8 +7,6 @@ export default defineNuxtPlugin((nuxt) => {
     const vuetify = createVuetify({
         ssr: true,
         blueprint: md3,
-        components,
-        directives,
     })
     nuxt.vueApp.use(vuetify);
 })

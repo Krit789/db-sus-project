58fbea485c21b61cd8250fbffe12540bfb3dabae
Jarukrit <jarukritsripaploen@protonmail.com>
2023-09-17 13:18:42 +0700

    Feat: Reservation now display data from server

--- Frontend/pages/management/reservation.vue
+++ Frontend/pages/management/reservation.vue
@@ -1,47 +1,28 @@
 <script lang="ts" setup>
-import {VDataTable} from "vuetify/labs/VDataTable";
-
-const {status, data, signIn, signOut} = useAuth();
-let dtData: any;
-async function reservations(token: any) {
-      // console.log(token)
-      // this.dtLoading = true;
-      useFetch(
-          "http://localhost:3000/proxy/api/control.php",
-          {
-            method: "POST",
-            body: {
-              "type": 9,
-              "token": data?.value.token,
-            },
-            lazy: true,
-            server: true
-          }
-      ).catch((error) => error).then(({status, message}) => {
-        dtData = message;
-        // this.dtLoading = false;
-      });
-    }
-
+import { VDataTable } from "vuetify/labs/VDataTable";
+const { status, data, signIn, signOut } = useAuth();
 </script>
+
 <script lang="ts">
 export default {
   data: () => ({
+    dtData: [],
+    dtLength: 0,
     itemsPerPage: 10,
-    dtLoading: false,
+    dtLoading: true,
     dtHeaders: [
       {
-        title: 'ID',
-        align: 'start',
-        sortable: false,
-        key: 'id',
+        title: "ID",
+        align: "start",
+        sortable: true,
+        key: "res_id",
       },
-      {title: 'User ID', align: 'end', key: 'userID'},
-      {title: 'Reserved On', align: 'end', key: 'create_time'},
-      {title: 'Reserved For', align: 'end', key: 'arrival'},
-      {title: 'Status', align: 'end', key: 'status'},
-      {title: 'No. of Customer', align: 'end', key: 'cus_count'},
-      {title: 'Table ID', align: 'end', key: 'table_id'},
+      { title: "User ID", align: "end", key: "user_id" },
+      { title: "Reserved On", align: "end", key: "create_time" },
+      { title: "Reserved For", align: "end", key: "arrival" },
+      { title: "Status", align: "end", key: "status" },
+      { title: "No. of Customer", align: "end", key: "cus_count" },
+      { title: "Table ID", align: "end", key: "table_id" },
     ],
     testPlacement: [
       {
@@ -52,7 +33,8 @@ export default {
         status: "INPROGRESS",
         cus_count: 5,
         table_id: 2,
-      }, {
+      },
+      {
         id: 2,
         userID: 2,
         create_time: "YYYY-MM-DD HH:MI:SS",
@@ -60,7 +42,8 @@ export default {
         status: "INPROGRESS",
         cus_count: 15,
         table_id: 1,
-      }, {
+      },
+      {
         id: 3,
         userID: 1,
         create_time: "YYYY-MM-DD HH:MI:SS",
@@ -70,74 +53,48 @@ export default {
         table_id: 1,
       },
     ],
-  })
-}
+  }),
+  methods: {
+    loadData() {
+      $fetch("/api/data", {
+        method: "POST",
+        body: {
+          type: 31,
+        },
+      })
+        .catch((error) => error.data)
+        .then(({ status, message }) => {
+          this.dtData = message;
+          this.dtLength = message.length;
+          this.dtLoading = false;
+        });
+    },
+  },
+};
 </script>
 <template>
   <Navbar>
     <v-main class="">
-      <h1 class="text-h3 font-weight-bold mt-8 ml-8 text-left">Reservation Management</h1>
-      <v-btn text="Click Me to fetch data table" @click="reservations"></v-btn>
+      <h1 class="text-h3 font-weight-bold mt-8 ml-8 text-left">
+        Reservation Management
+      </h1>
+      <v-btn text="Click Me to fetch data table" @click=""></v-btn>
       <v-sheet class="mt-8 ma-md-8 ma-xs-1 text-center" rounded="lg">
-        <v-data-table v-model:items-per-page="itemsPerPage"
-                      :headers="dtHeaders" :items="dtData" :loading="dtLoading"
-                      class="elevation-1" item-value="id"
-                      @click:row="(val, tabl) => { console.log(tabl.item.columns.id) }"></v-data-table>
+        <v-data-table
+          v-model:items-per-page="itemsPerPage"
+          :headers="dtHeaders"
+          :items="dtData"
+          :loading="dtLoading"
+          class="elevation-1"
+          item-value="id"
+          @update:options="loadData"
+          @click:row="
+            (val, tabl) => {
+              console.log(tabl.item.columns.id);
+            }
+          "
+        ></v-data-table>
       </v-sheet>
-      <!-- <v-table
-                fixed-header
-                height="auto"
-            >
-                <thead>
-                <tr>
-                    <th class="text-left">
-                    Reservation id
-                    </th>
-                    <th class="text-left">
-                    User
-                    </th>
-                    <th class="text-left">
-                    Count
-                    </th>
-                    <th class="text-left">
-                    Table
-                    </th>
-                    <th class="text-left">
-                    Created on
-                    </th>
-                    <th class="text-left">
-                    Arrival by
-                    </th>
-                    <th class="text-left">
-                    Status
-                    </th>
-                    <th class="text-left">
-                    Action
-                    </th>
-                </tr>
-                </thead>
-                <tbody>
-                <tr
-                    v-for="(item, index) in testPlacement"
-                    :key="index"
-                >
-                    <td class="text-left">{{ item.id }}</td>
-                    <template v-for="user in testuser">
-                        <td v-if="user.id == item.userID">{{ user.id }} : {{ user.first_name }} {{ user.last_name}}</td>
-                    </template>
-                    <td class="text-left">{{ item.cus_count }}</td>
-                    <td class="text-left">{{ item.table_id }}</td>
-                    <td class="text-left">{{ item.arrival }}</td>
-                    <td class="text-left">{{ item.create_time }}</td>
-                    <td class="text-left">{{ item.status }}</td>
-                    <td>
-                        <v-btn variant="text">
-                            Detail
-                        </v-btn>
-                    </td>
-                </tr>
-                </tbody>
-            </v-table> -->
     </v-main>
   </Navbar>
 </template>
---
+++ Frontend/server/api/data.ts
@@ -0,0 +1,28 @@
+import { getServerSession } from '#auth'
+
+export default defineEventHandler(async (event) => {
+    const session = await getServerSession(event); // Get user session
+    const body = await readBody(event) // Read from POST body
+    if (session?.token) {
+      
+      const reservations = await $fetch(
+        "http://localhost:3000/proxy/api/control.php",
+        {
+          method: "POST",
+          body: {
+            "type": body.type,
+            "token": session?.token
+          },
+        }
+    ).catch((error) => error.data);
+      // console.log(`Session Token: ${JSON.stringify(session?.token)}`);
+      // console.log(`Data: ${JSON.stringify(reservations)}`);
+      return reservations;
+    }
+    setResponseStatus(event, 418)
+    return {
+      status : 0,
+      message : 'Invalid Session Token'
+    }
+
+  })

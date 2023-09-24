3b6b1c65a2bdb42586b8c2b3d3651760a3701b3b
Jarukrit <jarukritsripaploen@protonmail.com>
2023-09-24 12:21:20 +0700

    Add Branch UI

--- Frontend/pages/management/branches/index.vue
+++ Frontend/pages/management/branches/index.vue
@@ -7,6 +7,7 @@ const route = useRouter();
 <script lang="ts">
 export default {
   data: () => ({
+    addBranch: false,
     dtSearch: "",
     dtErrorData: "",
     dtIsError: false,
@@ -57,13 +58,31 @@ export default {
 </script>
 <template>
   <v-main class="">
+    <v-dialog
+      width="auto"
+      v-model="addBranch"
+    >
+      <v-card width="400">
+        <v-card-title>Add Branch</v-card-title>
+        <v-card-text>
+          <v-text-field label="Name"></v-text-field>
+          <v-textarea label="Address"></v-textarea>
+          <v-text-field label="Open Time" type="time"></v-text-field>
+          <v-text-field label="Close Time" type="time"></v-text-field>
+        </v-card-text>
+        <v-card-actions>
+        <v-btn color="success" append-icon="mdi-plus" @click="">Add</v-btn>
+          <v-btn color="primary" @click="addBranch = false">Cancel</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
     <h1 class="text-h3 font-weight-bold my-8 ml-8 text-left">Branches Management</h1>
     <v-sheet class="mt-8 ma-md-8 ma-xs-1 text-center" rounded="lg">
       <v-alert v-if="dtIsError" class="ma-3" color="error" icon="$error" title="Fetch Error">{{ dtErrorData }}</v-alert>
       <v-btn :disabled="dtLoading" class="align-right mb-3" prepend-icon="mdi-refresh" text="Refresh"
              @click="loadData"></v-btn>
              <v-btn :disabled="dtLoading" color="success" class="ml-5 align-right mb-3" prepend-icon="mdi-plus" text="Add Branch"
-             @click="loadData"></v-btn>
+             @click="addBranch = true"></v-btn>
       <v-data-table
           v-model:items-per-page="itemsPerPage"
           :headers="dtHeaders"

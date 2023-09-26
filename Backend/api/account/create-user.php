fdb91581ea29c09ed4687a10db0ec05fb6a34bcf
Jarukrit <jarukritsripaploen@protonmail.com>
2023-09-26 23:14:36 +0700

    FIx API

--- Backend/api/account/create-user.php
+++ Backend/api/account/create-user.php
@@ -6,7 +6,6 @@ header('Access-Control-Allow-Origin:*');
 header('Access-Control-Allow-Method:POST');
 header("Access-Control-Allow-Headers: X-Requested-With");
 header('Content-Type:application/json');
-include '../random.php';
 include '../check.php';
 
 $obj = new Database();
